123dde02c7804051e84f85bbf2d322ce3b92c729
11Spades <77303844+11Spades@users.noreply.github.com>
2025-11-11 12:00:35 -0500

    chore: Resolve merge conflict

--- CommitCafe.java
+++ CommitCafe.java
@@ -3,14 +3,10 @@ public class CommitCafe {
         String barista = "nvf";
         int cups = 16;
 
-<<<<<<< HEAD
         // TODO: Simulate a merge conflict by editing this file.
         // Replace the println() below with your own message or pun.
         System.out.println(barista + " broke " + cups + " cups today.");
-=======
-        System.out.println("nvf brewed 16 cups today.");
         System.out.println("Commit your coffee, not your code before it compiles.");
->>>>>>> origin/main
     }
 }
 
