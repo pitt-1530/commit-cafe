7b0abbf75f541e6d72f912b2711f27d3b26bafc9
Hiuyeung Kong <hik40@pitt.edu>
2025-11-11 11:57:12 -0500

    customized CommitCafe message

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,11 +1,11 @@
 public class CommitCafe {
     public static void main(String[] args) {
-        String barista = "Base";
+        String barista = "Hiuyeung";
         int cups = 0;
 
         // TODO: Simulate a merge conflict by editing this file.
         // Replace the println() below with your own message or pun.
-        System.out.println(barista + " brewed " + cups + " cups today.");
+        System.out.println(barista + " brewed " + cups + " cups of coffee today.");
     }
 }
 
