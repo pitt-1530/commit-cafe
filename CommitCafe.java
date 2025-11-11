85146255a8b05471f5b66a8b5b88e69a2caea101
gaz0319 <167563925+gaz0319@users.noreply.github.com>
2025-11-11 17:21:20 +0000

    Resolve merge in CommitCafe.java

--- CommitCafe.java
+++ CommitCafe.java
@@ -3,14 +3,11 @@ public class CommitCafe {
         String barista = "nvf";
         int cups = 23;
 
-<<<<<<< HEAD
         // TODO: Simulate a merge conflict by editing this file.
         // Replace the println() below with your own message or pun.
         System.out.println(barista + " hellooo" + " world");
-=======
         System.out.println("nvf brewed 23 cups today.");
         System.out.println("Keep calm and merge the mocha.");
->>>>>>> origin/main
     }
 }
 
