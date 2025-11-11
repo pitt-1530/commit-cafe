35ed2a49e68cad11a0d92d69d95c46e33ba54203
Nadine von Frankenberg <vonfrankenberg@pitt.edu>
2025-11-11 11:48:40 -0500

    Instructor commit: Revised print statement

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,10 +1,9 @@
 public class CommitCafe {
     public static void main(String[] args) {
-        String barista = "Base";
-        int cups = 0;
+        String barista = "nvf";
+        int cups = 16;
 
-        // TODO: Simulate a merge conflict by editing this file.
-        // Replace the println() below with your own message or pun.
-        System.out.println(barista + " brewed " + cups + " cups today.");
+        System.out.println("nvf brewed 16 cups today.");
+        System.out.println("Commit your coffee, not your code before it compiles.");
     }
 }
