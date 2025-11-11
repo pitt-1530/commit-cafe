a6fa49329f4fe01221406a02e2c875314d2a3b28
Annie Dai <anniedwy03@gmail.com>
2025-11-11 12:44:24 -0500

    Resolve merge in CommitCafe.java

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,18 +1,13 @@
 public class CommitCafe {
     public static void main(String[] args) {
-<<<<<<< HEAD
         String barista = "Annie";
-        int cups = 3;
-
-        // small change for class exercise (merge-conflict bait)
-        System.out.println("taco tuesday");
-=======
-        String barista = "nvf";
+        String barista1 = "nvf";
         int cups = 23;
 
-        System.out.println("nvf brewed 23 cups today.");
+        // small change for class exercise (merge-conflict bait)
+        System.out.println(barista + " taco tuesday");
+        System.out.println(barista1 + " brewed" + cups + "cups today.");
         System.out.println("Keep calm and merge the mocha.");
->>>>>>> origin/main
     }
 }
 
