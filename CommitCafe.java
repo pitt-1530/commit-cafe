bdc6764195f1027529d61e11acfc6a51b9cc1b5a
zec20 <123476848+zec20@users.noreply.github.com>
2025-11-11 12:30:26 -0500

    Merge remote-tracking branch 'origin/main' into zezhong

--- CommitCafe.java
+++ CommitCafe.java
@@ -5,6 +5,7 @@ public class CommitCafe {
 
         // TODO: Simulate a merge conflict by editing this file.
         // Replace the println() below with your own message or pun.
+        System.out.println(barista + " brewed " + (cups + 5) + " cups with extra caffeine!");
         System.out.println(barista + " says: Keep calm and brew on!");
     }
 }
