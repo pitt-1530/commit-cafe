b0c65d7b67567ebae9cf3f967c8d1fcac9a031c2
Nadine von Frankenberg <vonfrankenberg@pitt.edu>
2025-11-10 21:16:01 -0500

    Reset: base CommitCafe.java

---
+++ CommitCafe.java
@@ -0,0 +1,10 @@
+public class CommitCafe {
+    public static void main(String[] args) {
+        String barista = "Base";
+        int cups = 0;
+
+        // TODO: Simulate a merge conflict by editing this file.
+        // Replace the println() below with your own message or pun.
+        System.out.println(barista + " brewed " + cups + " cups today.");
+    }
+}
