09267252893b0b37ede5347933000e5df8edf5c7
Yang Ye <yay112@pitt.edu>
2025-11-11 11:50:30 -0500

    this is a in class change for git

--- CommitCafe.java
+++ CommitCafe.java
@@ -5,8 +5,9 @@ public class CommitCafe {
 
         // TODO: Simulate a merge conflict by editing this file.
         // Replace the println() below with your own message or pun.
-        System.out.println(barista + " brewed " + cups + " cups today.");
+        System.out.println("this is a in class exercise");
     }
 }
 
+//this is a change test for in class exercise
 // Branch for Yang
